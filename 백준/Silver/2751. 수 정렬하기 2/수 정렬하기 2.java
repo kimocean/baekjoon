a61c8db1f37cc3254a4d6a0a7dd41e0e46390b64
kimocean <101934302+kimocean@users.noreply.github.com>
2024-01-22 10:43:55 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 688 ms, Memory: 125212 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,36 +2,35 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		int N = Integer.parseInt(br.readLine());
 		
-		List<Integer> list = new ArrayList<Integer>();
+		// 수가 중복되지 않아서 없으면 false(기본값) 있으면 true
+		// 0 : 1,000,000
+		boolean arr[] = new boolean[2000001];
 		
 		for(int i=0;i<N;i++) {
-			list.add(Integer.parseInt(br.readLine()));
+			// 입력값을 바로 arr의 인덱스로 잡아 줌
+			// 1을 입력받으면 arr[1000001] = true
+			arr[Integer.parseInt(br.readLine()) + 1000000] = true;
 		}
-		
-		Collections.sort(list);
 
-		for(int i=0;i<list.size();i++) {
-			sb.append(list.get(i)).append("\n");
+		// 어차피 인덱스 오름차순으로 순차 탐색하기 때문에 자동 오름차순 정렬으로 출력
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]) {
+				sb.append(i-1000000).append("\n");
+			}
 		}
 		
 		System.out.println(sb);
-
 	}
 }
-
-
-/*
-1. Arrays.sort() -> Collections.sort()
-2. Scanner -> BufferedReader
-3. sop -> StringBuilder
-*/
