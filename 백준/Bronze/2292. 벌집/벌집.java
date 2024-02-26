f64ac12131cb85f6a3d010b68db1b65024fd0ce7
kimocean <101934302+kimocean@users.noreply.github.com>
2024-02-26 10:52:11 +0900

    [Bronze II] Title: 벌집, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,36 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
 	
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();	
+		int N = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(br.readLine()); // 벌집 주소
-		int num = 1; // i의 역할.. 6이랑 곱해 줄 변수
-//		int cnt = 2; // 겹의 수?
-		int range = 2; // 최솟값 기준 범위(2, 9, 20..)
-		
+		int cnt = 1;
 		if(N == 1) {
 			System.out.println(1);
 		} else {
-			while(range <= N) {
-				range = range + 6*num;
-				num++;
+			while(N > 1) {
+				N -= 6*cnt;
+				cnt++;
 			}
-			System.out.println(num);
+			System.out.println(cnt);
 		}
 	}
 }
 
 /*
-
-1 -> 6 -> 12 -> 18 ... 6*i가 범위가 됨
-따라서 i++ 해 주면서 6과 곱한 걸 빼 주며 N이 음수가 될 때까지 while문 돌림
+1 : 1 / 1개
+2~7 : 2 / 6개
+8~19 : 3 / 12개
+20~37: 4 / 18개
+6의 0제곱(?), *1, *2...
+이걸 어떻게 식으로..
 
 */
