8002d30d79de23fdb908dd16fc753ec23cbcd19c
kimocean <101934302+kimocean@users.noreply.github.com>
2024-02-26 15:05:00 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static int factorial(int n) {
-		int res = 1;
-		for(int i=2;i<=n;i++) {
-			res *= i;
-		}
-		return res;
-	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
 		System.out.println(factorial(N) / (factorial(K) * factorial(N-K)));
+		
+	}
+	
+	static public int factorial(int a) {
+		if(a <= 1) {
+			return 1;
+		} else {
+			return a * factorial(a-1);
+		}
 	}
 }
