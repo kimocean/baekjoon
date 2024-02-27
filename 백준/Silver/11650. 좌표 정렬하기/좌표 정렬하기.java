f7c5bfd39a6bdf20192269cf3aa0afbfeb0983cc
kimocean <101934302+kimocean@users.noreply.github.com>
2024-02-27 16:03:17 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1768 ms, Memory: 60848 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -4,40 +4,34 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
+
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		int[][] arr = new int[N][2];
 		
 		for(int i=0;i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
 		Arrays.sort(arr, new Comparator<int[]>() {
 			@Override
-			public int compare(int[] arr1, int[] arr2) {
-				if(arr1[0] == arr2[0]) { // x좌표가 동일하면 y좌표 비교
-					return arr1[1] - arr2[1];
-				} else { // 동일하지 않으면 x좌표 비교
-					return arr1[0] - arr2[0];
+			public int compare(int i1[], int i2[]) {
+				if(i1[0] == i2[0]) {
+					return i1[1] - i2[1];
+				} else {
+					return i1[0] - i2[0];
 				}
 			}
 		});
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<2;j++) {
-				sb.append(arr[i][j] + " ");
-			}
-			sb.append("\n");
-		}
 
-		System.out.println(sb);
-		
+		for(int i=0;i<N;i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
 	}
 }
