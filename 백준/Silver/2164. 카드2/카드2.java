af4cfe80ea4706e09a2a70a7c084b142c46c732d
kimocean <101934302+kimocean@users.noreply.github.com>
2024-03-01 19:05:55 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45468 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,26 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;	
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		Queue<Integer> que = new LinkedList<>();
+		int N = Integer.parseInt(br.readLine());
+		Queue que = new LinkedList<Integer>();
 		
 		for(int i=1;i<=N;i++) {
 			que.add(i);
 		}
 		
 		while(que.size() > 1) {
-			que.poll(); // head 의 값 제거
-			que.add(que.poll()); // add 자체의 반환값은 tf
+			que.poll();
+			que.add(que.poll());
 		}
 		
-		System.out.println(que.peek()); // head 의 값 출력
+		System.out.println(que.peek());
+		
 	}
 }
