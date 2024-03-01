cb352e635dd1ef3d368492e2a8a92e91d230efc0
kimocean <101934302+kimocean@users.noreply.github.com>
2024-03-01 20:03:15 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;	
+import java.util.Stack;
 
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		for(int i=0;i<N;i++) {
-			Stack<Character> stack = new Stack<>();
-			String line = br.readLine();
-			
-			for(int j=0;j<line.length();j++) {
-				if(line.charAt(j) == '(') {
-					stack.push(line.charAt(j));
-				} else if(stack.isEmpty()) {
-					stack.push(')');
-					break;
+		int T = Integer.parseInt(br.readLine());
+		Stack<Character> st = new Stack<Character>();
+		String str = br.readLine();
+		boolean flag = true;
+		
+		for(int i=0;i<T;i++) {
+			for(int j=0;j<str.length();j++) {
+				if(str.charAt(j) == '(') {
+					st.add('(');
 				} else {
-					stack.pop();
+					if(st.isEmpty()) {
+						flag = false;
+						break;
+					} else {
+						st.pop();
+					}
 				}
 			}
 			
-			if(stack.isEmpty()) {
-				System.out.println("YES");
-			} else {
+			if(!flag || !st.isEmpty()) {
 				System.out.println("NO");
+			} else {
+				System.out.println("YES");
 			}
-				
+			
+			str = br.readLine();
+			flag = true;
+			st.clear();
+			
 		}
 	}
 }
