cfa0a696f5153f75c9d100e6e3593c4ed6a48dd7
kimocean <101934302+kimocean@users.noreply.github.com>
2024-03-23 19:13:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 316 ms, Memory: 85140 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,23 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
+	
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 //		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+	
 		int N = Integer.parseInt(br.readLine());
-		int cnt = 1;
-		int num = 666;
+		int cnt = 0;
+		int num = 0;
 		
-		while(cnt != N) {
-			num++;
-			if(String.valueOf(num).contains("666")) {
+		for(int i=666;i<Integer.MAX_VALUE;i++) {
+			String str = i + "";
+			if(str.contains("666")) {
 				cnt++;
+			}
+			if(cnt == N) {
+				num = i;
+				break;
 			}
 		}
 		
