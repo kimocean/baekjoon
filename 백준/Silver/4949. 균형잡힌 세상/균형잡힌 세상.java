7702ca7affeb79a8251e94645efad1b2ea7831e5
kimocean <101934302+kimocean@users.noreply.github.com>
2024-03-24 12:20:32 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 312 ms, Memory: 19408 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -4,45 +4,39 @@ import java.util.Stack;
 
 public class Main {
 	
-	public static String scan(String str) {
-		String result = "yes";
-		Stack<Character> st = new Stack<Character>();
-		for(int i=0;i<str.length();i++) {
-			char c = str.charAt(i);
-			if(c == '(' || c == '[') {
-				st.add(c);
-			} else if(c == ')') {
-				if(st.empty() || st.peek() != '(') {
-					return "no";
-				} else {
-					st.pop();
-				}
-			} else if(c == ']') {
-				if(st.empty() || st.peek() != '[') {
-					return "no";
-				} else {
-					st.pop();
-				}
-			}
-		}
-		
-		if(!st.isEmpty()) {
-			return "no";
-		} else {
-			return "yes";
-		}
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 //		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+
 		String str = br.readLine();
 		
 		while(!str.equals(".")) {
-			String result = scan(str);
-			System.out.println(result);
+			Stack<Character> stack = new Stack<>();
+			String yn = "yes";
+			for(int i=0;i<str.length();i++) {
+				char c = str.charAt(i);
+				
+				if(c == '(' || c == '[') {
+					stack.add(c);
+				} else if(c == ')' || c == ']') {
+					if(stack.isEmpty()) {
+						yn = "no";
+						break;
+					} else if(c == ')' && stack.peek() != '(' || c == ']' && stack.peek() != '[') {
+						yn = "no";
+						break;
+					} else {
+						stack.pop();
+					}
+				}
+			}
+			
+			if(!stack.isEmpty()) {
+				yn = "no";
+			}
+			
+			System.out.println(yn);
 			str = br.readLine();
 		}
 	}
