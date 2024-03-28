64f4a35ddd574c53da08bed2b0758de1d79e6635
kimocean <101934302+kimocean@users.noreply.github.com>
2024-03-28 22:28:59 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14360 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -11,23 +10,22 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 //		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-	
+
 		int N = Integer.parseInt(br.readLine());
 		String s = br.readLine();
 		int r = 31;
 		int M = 1234567891;
-		
-		int sum = 0;
+		long pow = 1;
+		long sum = 0;
 		
 		for(int i=0;i<N;i++) {
-			int num = s.charAt(i)-'a'+1;
-			int pow = 31;
+			int c = s.charAt(i) - 96;
 			
-			sum += (num % M) * ((int)Math.pow(r, i) % M);
-//			sum += num * (int)Math.pow(r, i) % M;
-//			pow = (int)Math.pow(r, i) % M;
-//			sum += (num % M) * pow;
+			sum += (c * pow) % M;
+			pow = (pow * r) % M;
 		}
-		System.out.println(sum);
+		
+		System.out.println(sum%M);
+		
 	}
 }
