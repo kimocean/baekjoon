b3bde147b1022fccc7b0bda56315cc8dffc28880
kimocean <101934302+kimocean@users.noreply.github.com>
2024-03-31 13:46:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Comparator;
 
 public class Main {
 	
@@ -11,24 +9,25 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 //		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 0;
 		
-		int N = Integer.parseInt(br.readLine()); // 설탕 kg
-		
-		int sum = N;
-		
-		for(int x=0;x<N;x++) {
-			for(int y=0;y<N;y++) {
-				if(5*x + 3*y == N) {
-					sum = Math.min(sum, x+y);
-				}
+		while(N > 0) {
+			if(N % 5 == 0) {
+				cnt += N / 5;
+				System.out.println(cnt);
+				return;
 			}
+			if(N < 3) {
+				System.out.println(-1);
+				return;
+			}
+			N -= 3;
+			cnt++;
 		}
 		
-		if(sum == N) {
-			System.out.println(-1);
-		} else {
-			System.out.println(sum);
-		}
+		System.out.println(cnt);
 		
 	}
 }
